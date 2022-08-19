b4382da2850c909c2c8be4f5a8abf8f198caa829
J-Tapio <skeletorcodes@gmail.com>
2022-08-19 17:59:37 +0200

    Add host information. Now should use '0.0.0.0'

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import server from "./app.js";
 const PORT = process.env.PORT || 3005;
 
-server.listen({port: PORT}, function (error, address) {
+server.listen({port: PORT, host: "0.0.0.0"}, function (error, address) {
   if (error) {
     server.log.error(error);
     process.exit(1); // Close-down the node process.
